255a5f2dc8ce1f1526740cf7456673db7832683c
Trevor R <tjdoescode@gmail.com>
2024-04-15 18:12:03 -0500

    feat: store messages on server, websocket send/receive

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-const ready = callback => {
-    if (document.readyState != "loading") callback()
-    else document.addEventListener("DOMContentLoaded", callback)
-}
+const ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+};
+
+let messages = [];
+
+const addMessage = (message) => {
+  const messageViewer = document.getElementById("message-viewer");
+  messages.push(message);
+  const newMessage = document.createElement("p");
+  newMessage.textContent = message;
+  messageViewer.appendChild(newMessage);
+};
 
 ready(() => {
-    const messageForm = document.getElementById('message-send-form')
-    const messageViewer = document.getElementById('message-viewer')
-    let messages = []
+  const socket = new WebSocket("ws://localhost:3000");
+  const messageForm = document.getElementById("message-send-form");
 
-    messageForm.addEventListener('submit', (event) => {
-        event.preventDefault()
-        const formData = new FormData(event.target)
-        const message = formData.get("message")
-        messages.push(message)
-        const newMessage = document.createElement('p')
-        newMessage.textContent = message
-        messageViewer.appendChild(newMessage)
-    })
-})
+  messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const message = formData.get("message");
+    addMessage(message);
+    socket.send(`sendMessage ${message}`);
+    console.log("sent");
+  });
+
+  socket.addEventListener("open", (event) => {
+    console.log("Socket opened");
+    socket.send("getAllMessages");
+  });
+
+  socket.addEventListener("message", (event) => {
+    let msg = String(event.data);
+    console.log(`Received: ${msg}`);
+    console.log(typeof msg);
+
+    if (msg.startsWith("allMessages")) {
+      let messages = msg.split(" ", 2)[1].split(",");
+      console.log(messages);
+
+      messages.forEach((message) => {
+        addMessage(message);
+      });
+    }
+  });
+
+  socket.addEventListener("error", (event) => {
+    console.error(`Error in socket: ${event}`);
+  });
+
+  socket.addEventListener("close", () => {
+    console.log("Socket closed");
+  });
+});
--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const expressWs = require('express-ws')(app);
+const expressWs = require("express-ws")(app);
+
+const messages = [];
 
 app.use(function (req, res, next) {
-  console.log('middleware');
-  req.testing = 'testing';
+  console.log("middleware");
+  req.testing = "testing";
   return next();
 });
 
-app.get('/', function(req, res, next){
-  console.log('get route', req.testing);
+app.get("/", function (req, res, next) {
+  console.log("get route", req.testing);
   res.end();
 });
 
-app.ws('/', function(ws, req) {
-  ws.on('message', function(msg) {
+app.ws("/", function (ws, req) {
+  ws.on("message", function (msg) {
     console.log(msg);
+    if (msg.startsWith("getAllMessages")) {
+      ws.send(`allMessages ${messages}`);
+    } else if (msg.startsWith("sendMessage")) {
+      let message = msg.split(" ")[1];
+      messages.push(message);
+      ws.send("acknowledged")
+    }
   });
-  console.log('socket', req.testing);
+  console.log("socket", req.testing);
 });
 
 app.listen(3000);
